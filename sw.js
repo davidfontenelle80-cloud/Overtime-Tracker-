989dc9de9ebee48275cadcedbea5ccd6f327d9ff
davidfontenelle80-cloud <davidfontenelle80@gmail.com>
2026-06-24 21:36:11 -0400

    chore: bump cache for requested settings back style

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  *  5. Broadcast RELOAD_READY to all clients after activation
  */
 
-const CACHE_VERSION = 'overtime-tracker-v20-half-settings-back';
+const CACHE_VERSION = 'overtime-tracker-v21-requested-settings-back';
 
 const PRECACHE_URLS = [
   './',
